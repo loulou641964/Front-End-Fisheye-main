f881fb5fc861ae7c6b4040b9e1a5e6c2cfb9fc0a
guy coste <guycoste86@gmail.com>
2024-05-08 17:53:46 +0200

    commit

--- Front-End-Fisheye-main/scripts/templates/photographer.js
+++ Front-End-Fisheye-main/scripts/templates/photographer.js
@@ -1,20 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait, price } = data;
-
+    const { name, city, country, portrait, price, tagline } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        const p =document.createElement("p")
-        p.textContent = price + "€/jour"
+        const location = document.createElement("h3");
+        location.textContent = `${city}, ${country}`;
+        const p = document.createElement("p");
+        p.textContent = price + "€/jour";
+        const taglineElt = document.createElement("p");
+        taglineElt.textContent = tagline;
+        taglineElt.classList.add("tagline");
+        
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(taglineElt);
         article.appendChild(p);
-        return (article);
+        
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM };
 }
+  
