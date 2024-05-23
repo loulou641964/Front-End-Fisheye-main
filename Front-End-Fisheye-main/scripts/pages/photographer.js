41f50b7f712dba7c866688efb2c81ca9fc5e3365
guy coste <guycoste86@gmail.com>
2024-05-23 18:42:13 +0200

    commit

--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -1,13 +1,13 @@
-import { getPhotographers } from "../utils/datahandling.js";
+import { getMediaByPhotographer, getPhotographers } from "../utils/datahandling.js";
 import { displayModal, closeModal } from "../utils/contactForm.js";
 const urlParams = new URLSearchParams(window.location.search);
 
-const id = urlParams.get('id');
+const id = parseInt( urlParams.get('id') );
 
 
 
 function findPhotographerById(photographers, id) {
-    return photographers.find(photographer => photographer.id === parseInt(id));
+    return photographers.find(photographer => photographer.id === id );
 }
 
 function displayPhotographerDetails(photographer) {
@@ -28,12 +28,21 @@ function displayPhotographerDetails(photographer) {
     photographerPrice.textContent = `${photographer.price}€/jour`;
     photographerImg.setAttribute("src", `assets/photographers/${photographer.portrait}`);
     photographerImg.setAttribute("alt", `${photographer.name}`);
+    
+    // Ajoute l'écouteur d'événements pour la redirection
+    photographerImg.addEventListener('click', () => {
+        window.location.href = `photographer.html?id=${photographer.id}`;
+    });
 }
+
+
 
 async function init() {
     const { photographers } = await getPhotographers();
     const photographer = findPhotographerById(photographers, id);
     displayPhotographerDetails(photographer);
+    const medias = await getMediaByPhotographer(id)
+    console.log (medias)
 }
 
 init();
