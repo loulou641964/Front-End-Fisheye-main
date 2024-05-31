4f3e9b8e2f576e8436f33e06312df02df97eb1d9
guy coste <guycoste86@gmail.com>
2024-05-31 22:14:51 +0200

    commit

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,8 +1,7 @@
+// Crée un conteneur pour les médias
 function createMediaContainer() {
     const mediaContainer = document.createElement('div');
     mediaContainer.classList.add('media-container');
     return mediaContainer;
 }
-
-
 
