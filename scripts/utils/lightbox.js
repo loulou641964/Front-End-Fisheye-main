5c3b430782651a438dac427d9d896ab5b6acca6d
guy coste <guycoste86@gmail.com>
2024-07-03 22:41:19 +0200

    commit

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,2 +1,23 @@
-
+function displayLightbox() {
+    const modal = document.getElementById('gallery_modal');
+    const modalBackground = document.getElementById('modal-background');
     
+    modal.style.display = 'block';
+    modal.setAttribute('aria-hidden', 'false');
+    modalBackground.style.display = 'block';
+
+ }
+    
+    
+
+// Fonction pour fermer le modal de contact
+function closeLightbox() {
+    const modal = document.getElementById('gallery_modal');
+    const modalBackground = document.getElementById('modal-background');
+    
+    modal.style.display = 'none';
+    modal.setAttribute('aria-hidden', 'true');
+    modalBackground.style.display = 'none';
+}
+    
+export { displayLightbox, closeLightbox };
