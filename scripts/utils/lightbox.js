cd8532235334424f2f4c37642c879fc15ca4bf8b
guy coste <guycoste86@gmail.com>
2024-07-16 17:58:49 +0200

    commit

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,29 +1,36 @@
 
+
 function displayLightbox() {
-    const modal = document.getElementById('gallery_modal');
-    const modalBackground = document.getElementById('modal-background');
-    
+    const modal = document.getElementById('lightbox');
     modal.style.display = 'block';
     modal.setAttribute('aria-hidden', 'false');
-    modalBackground.style.display = 'block';
+}
 
- }
 // Fonction pour fermer le modal de contact
 function closeLightbox() {
-    const modal = document.getElementById('gallery_modal');
-    const modalBackground = document.getElementById('modal-background');
-    
+    const modal = document.getElementById('lightbox');
     modal.style.display = 'none';
     modal.setAttribute('aria-hidden', 'true');
-    modalBackground.style.display = 'none';
 }
-function addListenersToGallery(){
-const galleryelts = document.querySelectorAll(".media-item")
-galleryelts.forEach(elt => {
-elt.addEventListener("click",displayLightbox)
-}) 
+
+function addListenersToGallery() {
+    const galleryElements = document.querySelectorAll(".media-item");
+    galleryElements.forEach(element => {
+        element.addEventListener("click", displayLightbox);
+    });
 }
-    
+
+document.addEventListener("DOMContentLoaded", () => {
+    addListenersToGallery();
+    document.getElementById('closeLightbox').addEventListener('click', closeLightbox);
+});
+
+   
+
+
 export { displayLightbox, closeLightbox, addListenersToGallery };
 
 
+    
+   
+   
