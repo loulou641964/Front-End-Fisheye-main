f34a347a2398de9b94b6ad9f29f1852e4d2a0013
guy coste <guycoste86@gmail.com>
2024-07-21 14:59:03 +0200

    commit

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,35 +1,50 @@
+let currentImageIndex = 0;
+let galleryElements = [];
 
-
-function displayLightbox(event,element) {
-    console.log(event)
-    console.log (element)
-   // document.querySelector ("#divMediaLightbox").appendChild  ( element )
-   document.querySelector ("#divMediaLightbox").innerHTML = `<img src = "${element.src } " alt = "${element.alt }" class ="lightbox-image"><p class ="lightbox-paragraphe">${element.alt}</p> `
-    const modal= document.getElementById('lightbox');
+function displayLightbox(event, element) {
+    currentImageIndex = galleryElements.indexOf(element);
+    updateLightbox();
+    const modal = document.getElementById('lightbox');
     modal.style.display = 'block';
     modal.setAttribute('aria-hidden', 'false');
 }
 
-// Fonction pour fermer le modal de contact
 function closeLightbox() {
     const modal = document.getElementById('lightbox');
     modal.style.display = 'none';
     modal.setAttribute('aria-hidden', 'true');
 }
 
+function updateLightbox() {
+    const element = galleryElements[currentImageIndex];
+    document.querySelector("#divMediaLightbox").innerHTML = `<img src="${element.src}" alt="${element.alt}" class="lightbox-image"><p class="lightbox-paragraphe">${element.alt}</p>`;
+}
+
+function showPreviousImage() {
+    currentImageIndex = (currentImageIndex > 0) ? currentImageIndex - 1 : galleryElements.length - 1;
+    updateLightbox();
+}
+
+function showNextImage() {
+    currentImageIndex = (currentImageIndex < galleryElements.length - 1) ? currentImageIndex + 1 : 0;
+    updateLightbox();
+}
+
 function addListenersToGallery() {
-    const galleryElements = document.querySelectorAll(".media-item");
+    galleryElements = Array.from(document.querySelectorAll(".media-item"));
     galleryElements.forEach(element => {
-        element.addEventListener("click", e=>{ displayLightbox(e,element) });
+        element.addEventListener("click", e => { displayLightbox(e, element); });
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     addListenersToGallery();
     document.getElementById('closeLightbox').addEventListener('click', closeLightbox);
+    document.getElementById('previousMedia').addEventListener('click', showPreviousImage);
+    document.getElementById('nextMedia').addEventListener('click', showNextImage);
 });
 
-   
+
 
 
 export { displayLightbox, closeLightbox, addListenersToGallery };
