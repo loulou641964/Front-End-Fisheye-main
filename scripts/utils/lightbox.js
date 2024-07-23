ab0252d7448189367fd3641c329e51dea0a5a860
guy coste <guycoste86@gmail.com>
2024-07-23 18:22:04 +0200

    commit

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,55 +1,74 @@
-let currentImageIndex = 0;
-let galleryElements = [];
+let currentMediaIndex = 0; // Index du média actuellement affiché dans la lightbox
+let galleryElements = []; // Tableau contenant tous les éléments de la galerie
 
+// Fonction pour afficher la lightbox
 function displayLightbox(event, element) {
-    currentImageIndex = galleryElements.indexOf(element);
-    updateLightbox();
-    const modal = document.getElementById('lightbox');
-    modal.style.display = 'block';
-    modal.setAttribute('aria-hidden', 'false');
+    currentMediaIndex = galleryElements.indexOf(element); // Met à jour l'index du média courant
+    updateLightbox(); // Met à jour le contenu de la lightbox
+    const modal = document.getElementById('lightbox'); // Récupère l'élément de la lightbox
+    modal.style.display = 'block'; // Affiche la lightbox
+    modal.setAttribute('aria-hidden', 'false'); // Met à jour l'attribut aria-hidden pour l'accessibilité
 }
 
+// Fonction pour fermer la lightbox
 function closeLightbox() {
-    const modal = document.getElementById('lightbox');
-    modal.style.display = 'none';
-    modal.setAttribute('aria-hidden', 'true');
+    const modal = document.getElementById('lightbox'); // Récupère l'élément de la lightbox
+    modal.style.display = 'none'; // Cache la lightbox
+    modal.setAttribute('aria-hidden', 'true'); // Met à jour l'attribut aria-hidden pour l'accessibilité
 }
 
+// Fonction pour mettre à jour le contenu de la lightbox
 function updateLightbox() {
-    const element = galleryElements[currentImageIndex];
-    document.querySelector("#divMediaLightbox").innerHTML = `<img src="${element.src}" alt="${element.alt}" class="lightbox-image"><p class="lightbox-paragraphe">${element.alt}</p>`;
+    const element = galleryElements[currentMediaIndex]; // Récupère l'élément courant de la galerie
+    let mediaHTML = ''; // Variable pour stocker le HTML du média
+
+    // Vérifie si l'élément est une image
+    if (element.tagName === 'IMG') {
+        mediaHTML = `<img src="${element.src}" alt="${element.alt}" class="lightbox-image">`; // Crée le HTML pour l'image
+    } 
+    // Vérifie si l'élément est une vidéo
+    else if (element.tagName === 'VIDEO') {
+        
+        const source = element.querySelector("source")
+        console.log(source)
+        mediaHTML = `
+            <video controls class="lightbox-video">
+                <source src="${source.src}" type="${source.type}">
+                Your browser does not support the video tag.
+            </video>`; // Crée le HTML pour la vidéo
+    }
+
+    // Met à jour le contenu de la lightbox avec le média et sa description
+    document.querySelector("#divMediaLightbox").innerHTML = mediaHTML + `<p class="lightbox-paragraphe">${element.alt}</p>`;
 }
 
-function showPreviousImage() {
-    currentImageIndex = (currentImageIndex > 0) ? currentImageIndex - 1 : galleryElements.length - 1;
-    updateLightbox();
+// Fonction pour afficher le média précédent
+function showPreviousMedia() {
+    currentMediaIndex = (currentMediaIndex > 0) ? currentMediaIndex - 1 : galleryElements.length - 1; // Met à jour l'index du média précédent
+    updateLightbox(); // Met à jour le contenu de la lightbox
 }
 
-function showNextImage() {
-    currentImageIndex = (currentImageIndex < galleryElements.length - 1) ? currentImageIndex + 1 : 0;
-    updateLightbox();
+// Fonction pour afficher le média suivant
+function showNextMedia() {
+    currentMediaIndex = (currentMediaIndex < galleryElements.length - 1) ? currentMediaIndex + 1 : 0; // Met à jour l'index du média suivant
+    updateLightbox(); // Met à jour le contenu de la lightbox
 }
 
+// Fonction pour ajouter des écouteurs d'événements à chaque élément de la galerie
 function addListenersToGallery() {
-    galleryElements = Array.from(document.querySelectorAll(".media-item"));
+    galleryElements = Array.from(document.querySelectorAll(".media-item")); // Récupère tous les éléments de la galerie
     galleryElements.forEach(element => {
-        element.addEventListener("click", e => { displayLightbox(e, element); });
+        element.addEventListener("click", e => { displayLightbox(e, element); }); // Ajoute un écouteur d'événement click à chaque élément
     });
 }
 
+// Ajoute des écouteurs d'événements une fois le DOM complètement chargé
 document.addEventListener("DOMContentLoaded", () => {
-    addListenersToGallery();
-    document.getElementById('closeLightbox').addEventListener('click', closeLightbox);
-    document.getElementById('previousMedia').addEventListener('click', showPreviousImage);
-    document.getElementById('nextMedia').addEventListener('click', showNextImage);
+    addListenersToGallery(); // Ajoute les écouteurs aux éléments de la galerie
+    document.getElementById('closeLightbox').addEventListener('click', closeLightbox); // Ajoute un écouteur pour fermer la lightbox
+    document.getElementById('previousMedia').addEventListener('click', showPreviousMedia); // Ajoute un écouteur pour afficher le média précédent
+    document.getElementById('nextMedia').addEventListener('click', showNextMedia); // Ajoute un écouteur pour afficher le média suivant
 });
 
-
-
-
+// Exporte les fonctions pour les utiliser dans d'autres modules
 export { displayLightbox, closeLightbox, addListenersToGallery };
-
-
-    
-   
-   
